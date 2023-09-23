e18205bb924cb2cfdc61144cbf8befd244f24c54
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2023-09-23 21:38:32 +0700

    Buoi6

---
+++ Buoi6/js/main.js
@@ -0,0 +1,44 @@
+// 6 Kieu du lieu co ban cua JS
+
+//---------- Number ------------------
+var age = 22;
+console.log("Your age", age);
+
+// --------- Boolean -----------------
+var isAdult = true;
+console.log("You are adult", isAdult);
+
+//---------- String ------------------
+var firstName = "Diep";
+console.log("Your name is" + firstName);
+
+// --------- null ---------------------
+var phone = null;
+console.log("Your phone number is", phone);
+
+//----------- undefined ----------------
+var address = undefined;
+console.log("Your address is", address);
+
+// ---------- array --------------------
+var books = ["Book 1", "All in love", "Book 3", 1];
+console.log("Your favorite books are", books);
+
+// --------- object --------------------
+var closedFriend = {
+                    full_name: "Nguyen Thi B",
+                    age: 23,
+                    address: null
+} 
+console.log("Your best friend's name", closedFriend.full_name);
+
+// ------ Cac pham vi cua bien ----------------
+// var: toan file JS hien tai
+// let: co pham vi hoat dong ro rang
+// const: hang so
+
+
+// -------- if - else -------------------------
+
+// -------- vong lap --------------------------
+
